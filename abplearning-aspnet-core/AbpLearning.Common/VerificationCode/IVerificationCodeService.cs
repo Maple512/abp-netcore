24681ecb7905cc8d6026597c3a5cb20ee6bf9a3c
Mplae512 <17376589512@163.com>
2019-01-14 22:30:46 +0800

    IVerificationCodeService

---
+++ abplearning-aspnet-core/AbpLearning.Common/VerificationCode/IVerificationCodeService.cs
@@ -0,0 +1,12 @@
+﻿namespace AbpLearning.Common.VerificationCode
+{
+    using System.IO;
+
+    /// <summary>
+    /// 验证码 服务 接口
+    /// </summary>
+    public interface IVerificationCodeService
+    {
+        MemoryStream Create(out string code, VerificationCodeType type = VerificationCodeType.Number, int length = 4);
+    }
+}
---
+++ abplearning-aspnet-core/src/AbpLearning.Web.Core/Controllers/VerificationController.cs
@@ -0,0 +1,20 @@
+﻿namespace AbpLearning.Web.Core.Controllers
+{
+    using Abp.Runtime.Caching;
+    using Common.VerificationCode;
+
+    public class VerificationController:AbpLearningControllerBase
+    {
+        private readonly IVerificationCodeService _codeService;
+
+        private readonly ICacheManager _cacheManager;
+
+        public VerificationController(IVerificationCodeService codeService, ICacheManager cacheManager)
+        {
+            _codeService = codeService;
+            _cacheManager = cacheManager;
+        }
+
+        // TODO:验证码
+    }
+}
